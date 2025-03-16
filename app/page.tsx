e0feee994de077a82ccb4349b902071da2b3ec00
dufour-treuhand <tillo@dufour-treuhand.ch>
2025-03-16 13:28:45 +0100

    Client deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 import "@ant-design/v5-patch-for-react-19";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 import { Button } from "antd";
 import { BookOutlined, CodeOutlined, GlobalOutlined } from "@ant-design/icons";
 import styles from "@/styles/page.module.css";
