902d7124e6f72d421f251433f0d2caeafa2214a4
khoshimov2018 <khoshimov2018@gmail.com>
2025-04-11 11:27:33 +0200

    updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  distDir: 'build',
+  // For deployment on Vercel, we need to use their expected defaults
+  output: process.env.VERCEL ? undefined : 'standalone',
+  distDir: process.env.VERCEL ? '.next' : 'build',
   typescript: {
     ignoreBuildErrors: true,
   },
